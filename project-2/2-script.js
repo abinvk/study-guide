3940841bf5bca2bc7bf78f43c7ce15370cf2507a
Abin vk <vettikkadanabin@gmail.com>
2025-12-20 20:42:26 +0530

    for study

---
+++ project-2/2-script.js
@@ -0,0 +1,14 @@
+const button1=
+document.getElementById('addJs');
+const message1 =
+document.getElementById('msgg');
+document.addEventListener('click',() =>{
+    message1.textContent = 'added'
+});
+const button2 =
+document.getElementById('removeJs');
+const message2 =
+document.getElementById('msgg1');
+document.addEventListener('click',()=>{
+    message2.textContent = 'your left'
+});
